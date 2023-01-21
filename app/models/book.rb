c6675797150f04bf0f250b958e585766f8dbe7ae
udaikue <ikueikue@gmail.com>
2023-01-21 14:46:16 +0900

    books コントローラーを作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class Book < ApplicationRecord
   validates :title, presence: true
-  validates :status, inclusion: { in: %w(want stack wip done) }
+  validates :status, inclusion: { in: %w[want stack wip done] }
 end
