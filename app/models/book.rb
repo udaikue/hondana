7d9d32d64a027a0d9137bf1890244900d13f4654
udaikue <ikueikue@gmail.com>
2023-01-22 16:19:13 +0900

    一覧画面を作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :status, inclusion: { in: %w[want stack wip done] }
+
+  scope :organize, ->(progress) { where(status: progress).all }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@
 
 Book.create!(
   [
-    { title: 'いかにして問題をとくか', status: 'wip'},
-    { title: 'マスタリング TCP/IP', status: 'stack'},
-    { title: 'UML モデリング入門', status: 'wip'},
-    { title: 'ちょうぜつソフトウェア設計入門', status: 'wip'},
-    { title: '独習 PHP', status: 'wip'},
-    { title: 'Web を支える技術', status: 'done'},
-    { title: 'パーフェクト Ruby on Rails', status: 'done'},
-    { title: 'Clean Agile', status: 'want'},
-    { title: 'プリンシプルオブプログラミング', status: 'want'},
-    { title: 'アルゴリズム図鑑', status: 'want'},
+    { title: 'いかにして問題をとくか', status: 'wip' },
+    { title: 'マスタリング TCP/IP', status: 'stack' },
+    { title: 'UML モデリング入門', status: 'wip' },
+    { title: 'ちょうぜつソフトウェア設計入門', status: 'wip' },
+    { title: '独習 PHP', status: 'wip' },
+    { title: 'Web を支える技術', status: 'done' },
+    { title: 'パーフェクト Ruby on Rails', status: 'done' },
+    { title: 'Clean Agile', status: 'want' },
+    { title: 'プリンシプルオブプログラミング', status: 'want' },
+    { title: 'アルゴリズム図鑑', status: 'want' }
   ]
 )
